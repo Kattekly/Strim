03024e54412c517e961c59143f8e177eebffe066
Ekaterina <kattekly@mail.ru>
2022-09-20 16:57:50 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const people = [
+  {
+    name: 'James',
+    age: 31,
+  },
+  {
+    name: 'John',
+    age: 45,
+  },
+  {
+    name: 'Paul',
+    age: 65,
+  },
+  {
+    name: 'Ringo',
+    age: 49,
+  },
+  {
+    name: 'George',
+    age: 34,
+  }
+];
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        {people.filter(person => person.age < 60).map(filteredPerson => (
+            <li>
+              {filteredPerson.name}
+            </li>
+        ))}
+      </div>
   );
 }
 
