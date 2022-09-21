e228d4abcd71ed36232483296587f590479722c6
Ekaterina <kattekly@mail.ru>
2022-09-21 21:45:50 +0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const people = [
-  {
-    name: 'James',
-    age: 31,
-  },
-  {
-    name: 'John',
-    age: 45,
-  },
-  {
-    name: 'Paul',
-    age: 65,
-  },
-  {
-    name: 'Ringo',
-    age: 49,
-  },
-  {
-    name: 'George',
-    age: 34,
-  }
-];
 
 function App() {
   return (
       <div>
-        {people.filter(person => person.age < 60).map(filteredPerson => (
-            <li>
-              {filteredPerson.name}
-            </li>
-        ))}
+
       </div>
   );
 }
