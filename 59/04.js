36c629a0e3c5db050d9f2c7b2394b604bfa3aa0c
Ekaterina <kattekly@mail.ru>
2022-10-05 21:02:17 +0300

    lessons-4

--- 59/04.js
+++ 59/04.js
@@ -17,8 +17,15 @@ const todoList = [
 ]
 
 const tasks = {
-    [todoListId1]: [],
-    [todoListId2]: [],
+    [todoListId1]:
+        [{id: "21", title: "Rer", isDone: false},
+        {id: "23", title: "oooo", isDone: false},
+        {id: "109", title: "ddd", isDone: false}
+    ],
+    [todoListId2]: [{id: "15", title: "ffff", isDone: false},
+        {id: "243", title: "aass", isDone: false},
+        {id: "1509", title: "llll", isDone: false}],
         ["user name"]: "Bob"
 }
-console.log(tasks["user name"])
+console.log([...tasks[todoListId1], {id: "14", title: "REDAX", isDone: false}])
+console.log(tasks[todoListId1].find(t => t.id === "21"))
