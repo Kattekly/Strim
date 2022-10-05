ff661179954007041e2c100037b31937bf33f2cb
Ekaterina <kattekly@mail.ru>
2022-10-05 21:27:31 +0300

    lessons-4

--- 59/04.js
+++ 59/04.js
@@ -22,7 +22,7 @@ const tasks = {
         {id: "23", title: "oooo", isDone: false},
         {id: "109", title: "ddd", isDone: false}
     ],
-    [todoListId2]: [{id: "15", title: "ffff", isDone: false},
+    [todoListId2]: [{id: "15", title: "bluat", isDone: false},
         {id: "243", title: "aass", isDone: false},
         {id: "1509", title: "llll", isDone: false}],
         ["user name"]: "Bob"
@@ -31,3 +31,6 @@ console.log([...tasks[todoListId1], {id: "14", title: "REDAX", isDone: false}])
 console.log(tasks[todoListId1].find(t => t.id === "21"))
 console.log(tasks[todoListId1][2].title)
 console.log(tasks[todoListId1][2]["title"])
+console.log(tasks[todoList[1]["id"]][0].title)
+
+const number = [1, 2, 3, 4 ,5]
