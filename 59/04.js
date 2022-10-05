9881155b0468bf8f96ca5388fa1c5b1cdcab78ff
Ekaterina <kattekly@mail.ru>
2022-10-05 20:27:21 +0300

    lessons-4

--- 59/04.js
+++ 59/04.js
@@ -1,4 +1,14 @@
 const todoList = [
-    {id: "1", title: "What to learn", filter: "all"},
-    {id: "2", title: "What to buy", filter: "all"}
+    {
+        id: "1",
+        title: "What to learn",
+        filter: "all",
+        tasks: []
+    },
+    {
+        id: "2",
+        title: "What to buy",
+        filter: "all",
+        tasks: []
+    }
 ]
