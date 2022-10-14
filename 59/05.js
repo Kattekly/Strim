5d4598d66d7c35d392a8593016ee32f56284c0d8
Ekaterina <kattekly@mail.ru>
2022-10-14 18:45:19 +0300

    lessons-5

--- 59/05.js
+++ 59/05.js
@@ -1,3 +1,30 @@
 const num = [1, 56, 89, 234, 0, 911, -45, -690]
-const compare = (a, b) => a-b
+const compare = (a, b) => b-a
 console.log(num.sort(compare))
+
+let students = [
+    {
+        name: "Bob",
+        age: 22,
+        isMarried: true,
+        scores: 85
+    },
+    {
+        name: "Alex",
+        age: 21,
+        isMarried: true,
+        scores: 89
+    },
+    {
+        name: "Nick",
+        age: 20,
+        isMarried: false,
+        scores: 120
+    },
+    {
+        name: "John",
+        age: 25,
+        isMarried: false,
+        scores: 100
+    }
+];
